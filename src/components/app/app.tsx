6c7e325c6a668055f09ba211ffcea08e3202652e
MedusCode <87340589+MedusCode@users.noreply.github.com>
2024-02-26 03:33:59 -0800

    add: button component + normalize.css

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './app.module.css';
+import Button from '../button/button';
 
 function App() {
   return (
     <div>
       <p>Hello</p>
+      <Button>Button</Button>
     </div>
   );
 }
