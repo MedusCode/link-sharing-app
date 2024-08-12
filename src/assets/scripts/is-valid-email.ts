1e2736922940ecd0bb79a996dc4cfb4f87cf50cc
MedusCode <87340589+MedusCode@users.noreply.github.com>
2024-08-13 03:59:22 +0500

    add: signup page + useTextInput hook

--- src/assets/scripts/is-valid-email.ts
+++ src/assets/scripts/is-valid-email.ts
@@ -1,7 +1,7 @@
-const validateEmail = (email: string): boolean => {
+const isValidEmail = (email: string): boolean => {
   const isMailAtValid: boolean = !(email.indexOf('@') <= 0 || email.indexOf('@') !==  email.lastIndexOf('@'));
   const isMailDotValid: boolean = !(email.lastIndexOf('.') <= email.indexOf('@') + 1 || email.lastIndexOf('.') === email.length - 1);
   return isMailAtValid && isMailDotValid
 }
 
-export default validateEmail;
+export default isValidEmail;
